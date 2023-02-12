4940fcfb428f5204c101eadbf287fb3230cee9e1
Aleksandr Putilin <putilin93@gmail.com>
2023-02-12 14:38:28 +0600

    9th step of the project

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import brain_games.scripts.engine
+import random
+import math
+
+
+question = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+def game_question():
+    global num
+    num = random.randint(2, 100)
+    print(f'Question: {num}')
+
+
+def is_correct_answer():
+    top_devider = math.trunc(math.sqrt(num))
+    if top_devider > 1:
+        for i in range(2, top_devider + 1):
+            if (num % i) == 0:
+                correct_answer = 'no'
+                break
+            else:
+                correct_answer = 'yes'
+    else:
+        correct_answer = 'yes'
+    return correct_answer
+
+def main():
+    brain_games.scripts.engine.greet()
+    brain_games.scripts.engine.ask_question(question)
+    brain_games.scripts.engine.run_game(game_question, is_correct_answer)
+
+if __name__ == "__main__":
+    main()
